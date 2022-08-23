8d6163aa45d60f97c5eafec8cb48ed69c588dd2a
m_mitin <maximus405@gmail.com>
2022-08-24 00:52:33 +0300

    Initial Commit

---
+++ Dashboard_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dashboard_test
+//
+//  Created by Maxim Mitin on 24.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dashboard_test/Dashboard_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dashboard_testApp.swift
+//  Dashboard_test
+//
+//  Created by Maxim Mitin on 24.08.22.
+//
+
+import SwiftUI
+
+@main
+struct Dashboard_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
